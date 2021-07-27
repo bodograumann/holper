e958ba9319c61811b120f1faff7ee90fa8e2b557
Bodo Graumann <mail@bodograumann.de>
2021-07-27 16:22:52 +0200

    Cli, refactor: Use session as context manager

--- holper/cli.py
+++ holper/cli.py
@@ -22,70 +22,68 @@ db_file_opt = typer.Option(xdg_data_home() / "holper" / "data.sqlite")
 def events(db_file: str = db_file_opt):
     """Show list of all events"""
     Path(db_file).parent.mkdir(parents=True, exist_ok=True)
-    session = core.open_session(f"sqlite:///{db_file}")
+    with core.open_session(f"sqlite:///{db_file}") as session:
+        event_list = [
+            event for (event,) in session.execute(sqlalchemy.select(model.Event))
+        ]
 
-    event_list = [event for (event,) in session.execute(sqlalchemy.select(model.Event))]
+        if not event_list:
+            typer.echo("There are no existing events.")
 
-    if not event_list:
-        typer.echo("There are no existing events.")
-
-    for event in event_list:
-        typer.echo(
-            f"#{event.event_id} "
-            f"{event.start_time} - {event.end_time}: "
-            f"{event.name}, {len(event.races)} races, {len(event.categories)} categories, {len(event.entries)} entries"
-        )
-
-    session.close()
+        for event in event_list:
+            typer.echo(
+                f"#{event.event_id} "
+                f"{event.start_time} - {event.end_time}: "
+                f"{event.name}, "
+                f"{len(event.races)} races, {len(event.categories)} categories, {len(event.entries)} entries"
+            )
 
 
 @app.command()
 def new_event(name: str, date: datetime, db_file: str = db_file_opt):
     """Create a new single-race solo event"""
     Path(db_file).parent.mkdir(parents=True, exist_ok=True)
-    session = core.open_session(f"sqlite:///{db_file}")
-    event = model.Event(name=name, start_time=date, end_time=date + timedelta(days=1))
-    race = model.Race(first_start=date)
-    event.races.append(race)
+    with core.open_session(f"sqlite:///{db_file}") as session:
+        event = model.Event(
+            name=name, start_time=date, end_time=date + timedelta(days=1)
+        )
+        race = model.Race(first_start=date)
+        event.races.append(race)
 
-    session.add(event)
-    session.commit()
+        session.add(event)
+        session.commit()
 
-    typer.echo(
-        f"A new event “{name}” was created successfully with number #{event.event_id}."
-    )
-
-    session.close()
+        typer.echo(
+            f"A new event “{name}” was created successfully with number #{event.event_id}."
+        )
 
 
 @app.command()
 def import_categories(event_id: int, category_file: Path, db_file: str = db_file_opt):
     """Import event categories in IOF XML v3 format"""
-    session = core.open_session(f"sqlite:///{db_file}")
-    try:
-        (event,) = next(
-            session.execute(
-                sqlalchemy.select(model.Event).where(model.Event.event_id == event_id)
+    with core.open_session(f"sqlite:///{db_file}") as session:
+        try:
+            (event,) = next(
+                session.execute(
+                    sqlalchemy.select(model.Event).where(
+                        model.Event.event_id == event_id
+                    )
+                )
             )
-        )
-    except StopIteration:
-        typer.echo("Event could not be found.")
-        session.close()
-        return
+        except StopIteration:
+            typer.echo("Event could not be found.")
+            return
 
-    if event.categories:
-        typer.echo(f"Event #{event_id} already contains categories.")
-        session.close()
-        return
+        if event.categories:
+            typer.echo(f"Event #{event_id} already contains categories.")
+            return
 
-    with open(category_file) as stream:
-        categories = list(iofxml3.read(stream))
-    event.categories.extend(categories)
-    session.commit()
+        with open(category_file) as stream:
+            categories = list(iofxml3.read(stream))
+        event.categories.extend(categories)
+        session.commit()
 
-    typer.echo(f"Imported {len(categories)} categories")
-
-    session.close()
+        typer.echo(f"Imported {len(categories)} categories")
 
 
 if __name__ == "__main__":
